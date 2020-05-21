e35e5da82fe10f3302f25831756efa840eb4d10f
dKab <dkabardinov@gmail.com>
2020-05-22 01:54:55 +0300

    Fix after-install script

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu/website/
-npm instal
+npm install
 npm start
 parameters=$(aws ssm get-parameters --region us-east-2 --names CLOUDFLARE_X_AUTH_KEY CLOUDFLARE_X_AUTH_EMAIL CLOUDFLARE_ZONE_ID --with-decryption)
 PARAMETERS=$parameters node purge-cdn-cache.js
