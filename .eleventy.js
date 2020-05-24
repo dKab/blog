d2844bba515412d1b25913e958be2639732b6831
dKab <dkabardinov@gmail.com>
2020-05-24 14:51:11 +0300

    Staticman integration (wip)

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,9 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addFilter("cssmin", function(code) {
     return new CleanCSS({}).minify(code).styles;
   });
+  eleventyConfig.addFilter('jsonify', function (variable) {
+    return JSON.stringify(variable);
+  });
   eleventyConfig.addPassthroughCopy('assets');
   return {
     passthroughFileCopy: true
