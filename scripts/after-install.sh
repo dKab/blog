3e3f9ebd2b981ef5b1579602b667cb9b152c13f7
dKab <dkabardinov@gmail.com>
2020-06-06 17:11:45 +0300

    Purge cache before remove node_modules

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -2,9 +2,8 @@
 cd /home/ubuntu/deployment/website/
 npm install
 npm start
+parameters=$(aws ssm get-parameters --region us-east-2 --names CLOUDFLARE_X_AUTH_KEY CLOUDFLARE_X_AUTH_EMAIL CLOUDFLARE_ZONE_ID --with-decryption)
+PARAMETERS=$parameters node purge-cdn-cache.js
 rm -rf /home/ubuntu/deployment/website/node_modules
 rm -rf /home/ubuntu/website
 mv /home/ubuntu/deployment/website /home/ubuntu
-cd /home/ubuntu/website
-parameters=$(aws ssm get-parameters --region us-east-2 --names CLOUDFLARE_X_AUTH_KEY CLOUDFLARE_X_AUTH_EMAIL CLOUDFLARE_ZONE_ID --with-decryption)
-PARAMETERS=$parameters node purge-cdn-cache.js
